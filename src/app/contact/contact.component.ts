2144f7e531446051696cf9ee75dd8bacb9eb1f8c
Sujan99969 <dsujan999@gmail.com>
2022-09-18 09:51:58 -0700

    Add all my files

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  // confimmsg = document.getElementById("demo")
+  constructor() { }
+ 
+    // onclick(){
+    //   this.confimmsg.textContent="validating";
+    //     }
+  
+  ngOnInit() {
+  }
+
+}
